3c3a2ad77b2ac0a41ecb2254a23f853a85cca796
Vladimir Beev <vladimir.beev89@gmail.com>
2023-11-12 12:21:49 +0200

    Add some test with Ajax, XML, Javascript

---
+++ RSMotors/wwwroot/js/test.js
@@ -0,0 +1,24 @@
+﻿async function loadXmlDoc() {
+
+    try {
+        let url = "https://localhost:7049/Customer/GetNames";
+
+        let respons = await fetch(url);
+
+        if (respons.ok) {
+
+            let jsonn = await respons.json();
+
+            let arr = jsonn.value;
+
+            for (let i = 0; i < arr.length; i++) {
+                document.getElementById('xmlDoc').innerHTML = arr[i];
+            }
+        }
+        else {
+            document.getElementById('xmlDoc').innerHTML = "hahahah";
+        }
+    } catch (err) {
+        console.log(Error);
+    }
+};
